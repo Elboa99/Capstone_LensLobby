103a79b3fae0571ea40a8bdd367542d690bba2f1
Kenny Boateng <kennyboateng.99@gmail.com>
2024-10-17 12:38:46 +0200

    Revert "Creazione Package Security"
    This reverts commit 17ac34b45e80d96be3eca54a88ec93ef1798321e.

--- src/main/java/kennyboateng/Capstone_LensLobby/security/Config.java
+++ src/main/java/kennyboateng/Capstone_LensLobby/security/Config.java
@@ -1,16 +1,5 @@
 package kennyboateng.Capstone_LensLobby.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -21,18 +10,18 @@ public class Config {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtCheckFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return httpSecurity.build();
-    }
-
-
-    @Bean
-    PasswordEncoder getBCrypt() {
-        return new BCryptPasswordEncoder(11);
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .addFilterBefore(jwtCheckFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return httpSecurity.build();
+//    }
+//
+//
+//    @Bean
+//    PasswordEncoder getBCrypt() {
+//        return new BCryptPasswordEncoder(11);
 
     }
 }
