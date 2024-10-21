c23b6715ee6bc9d7ac9ec0cc5263cc336fe21602
Kenny Boateng <kennyboateng.99@gmail.com>
2024-10-21 17:09:27 +0200

    refactor repository Fotografo e Immagine

--- src/main/java/kennyboateng/Capstone_LensLobby/repositories/FotografoRepository.java
+++ src/main/java/kennyboateng/Capstone_LensLobby/repositories/FotografoRepository.java
@@ -7,12 +7,11 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface FotografoRepository extends JpaRepository<Fotografo, Long> {
 
-    Optional<Fotografo> findByEmail(String email);
-    List<Fotografo> findByNomeContainingIgnoreCase(String nome);
-    List<Fotografo> findByNomeUtenteContainingIgnoreCase(String nomeUtente);
+    @Repository
+    public interface FotografoRepository extends JpaRepository<Fotografo, Long> {
+        Optional<Fotografo> findByEmail(String email);
+    }
 
 
-}
+
