e5689d46bb6ec453d82e6cd1cc41b56317a6b072
Kenny Boateng <kennyboateng.99@gmail.com>
2024-10-30 02:57:36 +0100

    Update CapstoneLensLobbyApplication.java

--- src/main/java/kennyboateng/Capstone_LensLobby/CapstoneLensLobbyApplication.java
+++ src/main/java/kennyboateng/Capstone_LensLobby/CapstoneLensLobbyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapstoneLensLobbyApplication {
+public class  CapstoneLensLobbyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneLensLobbyApplication.class, args);
